dd084f02ca0030b93ffe5d4421872d586ed7ce55
DizonAj16 <150583871+DizonAj16@users.noreply.github.com>
2024-02-19 23:43:07 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -62,5 +62,5 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   </nav>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
+<h1>hello world</h1>
 </html>
